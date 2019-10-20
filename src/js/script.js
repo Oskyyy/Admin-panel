d624cd240be41523867cc09e8efd94a0d23fff3d
Oskar Wrotniak <oskarwro@gmail.com>
2019-10-20 14:39:02 +0200

    Add banners and styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,44 @@
+// Hamburger
+
 function toggleSidebar(visible) {
   document.querySelector('.sidebar').classList.toggle('toggled', visible);
 }
-  
+
 function toggleGeneral(wide) {
-  document.querySelector('.general').classList.toggle('wide', wide);
+  document.querySelector('.main').classList.toggle('wide', wide);
 }
-  
+
 document.querySelector('.hamburger').addEventListener('click', function(e) {
   e.preventDefault();
   toggleSidebar();
   toggleGeneral();
 });
+
+// Highlight current page
+
+var menuPages = document.getElementsByClassName('menu-link');
+
+for (var i = 0; i < menuPages.length; i++) {
+  menuPages[i].addEventListener('click', function() {
+    var current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
+  });
+}
+
+// Pages toggling
+for (var j = 0; j < menuPages.length; j++) {
+  menuPages[j].addEventListener('click', function() {
+    var menuPages = this.getAttribute('href');
+    var content = document.querySelector(menuPages + '-section');
+    console.log('content:', content);
+    var all = document.querySelectorAll('.subpage');
+
+    if (content) {
+      for (var k = 0; k < all.length; k++) {
+        all[k].classList.add('hidden');
+      }
+      content.classList.remove('hidden');
+    }
+  });
+}
