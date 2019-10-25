2075cbfdd803967871f6e297afeeedd8742f1c4f
Oskar Wrotniak <oskarwro@gmail.com>
2019-10-25 20:34:38 +0200

    In progress Adding docs

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+'use strict';
+
+// Check screen width (mobile)
+if (window.screen.width < 576) {
+  document.querySelector('.main').classList.add('wide');
+  document.querySelector('.sidebar').classList.add('toggled');
+}
+
 // Hamburger
 function toggleSidebar(visible) {
   document.querySelector('.sidebar').classList.toggle('toggled', visible);
@@ -39,6 +49,44 @@ for (var j = 0; j < menuPages.length; j++) {
     }
   });
 }
+
+// Chart
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // 1 (chart type)
+  type: 'bar',
+  data: {
+    // 2 (labels for the X axis)
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3 (data series setting)
+    datasets: [
+      {
+        // 4 (naming the data series)
+        label: 'Signups',
+        // 5 (data series color selection)
+        backgroundColor: '#8DBEC8',
+        borderColor: '#8DBEC8',
+        // 6 (supplement with sample data)
+        data: [346, 211, 232, 362, 419, 391, 289, 269, 278, 196]
+      },
+      {
+        label: 'FTD',
+        backgroundColor: '#F29E4E',
+        borderColor: '#F29E4E',
+        data: [411, 168, 294, 273, 471, 122, 187, 512, 324, 203]
+      },
+      {
+        label: 'Earned',
+        backgroundColor: '#71B374',
+        borderColor: '#71B374',
+        data: [378, 189, 263, 317, 445, 256, 238, 390, 301, 199],
+        // 7
+        hidden: true
+      }
+    ]
+  }
+});
 
 // Modals
 function closeModal() {
@@ -92,6 +140,23 @@ document.getElementById('go_chat').addEventListener('click', function() {
 });
 
 // Modals - ADD URL
-document.getElementById('go_addurl').addEventListener('click', function() {
+document.getElementById('go_addurl_1').addEventListener('click', function() {
   openModal('#modal_addurl');
+});
+document.getElementById('go_addurl_2').addEventListener('click', function() {
+  openModal('#modal_addurl');
+});
+document.getElementById('go_addurl_3').addEventListener('click', function() {
+  openModal('#modal_addurl');
+});
+document.getElementById('go_addurl_4').addEventListener('click', function() {
+  openModal('#modal_addurl');
+});
+
+// Modals - ADD BANNER
+document.getElementById('go_addbanner_1').addEventListener('click', function() {
+  openModal('#modal_addbanner');
+});
+document.getElementById('go_addbanner_2').addEventListener('click', function() {
+  openModal('#modal_addbanner');
 });
